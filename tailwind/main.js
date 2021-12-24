b0af65d5421424df855982d4693bff45f60d76d0
OaH <81689054+oah1006@users.noreply.github.com>
2021-12-24 17:40:13 +0700

    add register/login and content-language

---
+++ tailwind/main.js
@@ -0,0 +1,43 @@
+
+// HIDE FORM
+function hideForm() {
+    $('.modal').addClass('hidden');
+}
+
+$('.btn-back').click(function() {
+    hideForm();
+});
+
+// Handle sign in/ sign up
+
+function showFormSignUp() {
+    $('.modal').removeClass("hidden");
+    $('.sign-in').addClass("hidden");
+    $('.sign-up').removeClass("hidden");
+}
+
+function showFormSignIn() {
+    $('.modal').removeClass("hidden");
+    $('.sign-in').removeClass("hidden");
+    $('.sign-up').addClass("hidden");
+}
+
+// Sign up
+$('.btn-sign-up').click(function() {
+    showFormSignUp();
+});
+
+$('.btn-switch-register').click(function() {
+    showFormSignUp();
+});
+
+// Sign in
+
+$('.btn-sign-in').click(function() {
+    showFormSignIn();
+});
+
+$('.btn-switch-login').click(function() {
+    showFormSignIn();
+});
+
